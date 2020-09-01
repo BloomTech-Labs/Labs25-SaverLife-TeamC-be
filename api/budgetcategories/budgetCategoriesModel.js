0619e6793ce7cbce48a7ec1a63c58a86513cde7c
Victoria  Topham <topham.victoria@gmail.com>
2020-09-01 12:35:38 -0700

    outlined budgetCategoriesModel

--- api/budgetcategories/budgetCategoriesModel.js
+++ api/budgetcategories/budgetCategoriesModel.js
@@ -1,5 +1,39 @@
 const db = require('../../config/dsConfig.js')
 
 module.exports = {
-    
+    getLineById,
+    getAllByBudget,
+    addLine,
+    updateLine,
+    deleteLine
+}
+
+
+
+//allow a user to read a specific budget line
+
+function getLineById() {
+    return null
+}
+
+//allow a user to read all budget lines by budget id
+
+function getAllByBudget() {
+    return null
+}
+
+//allow a user to create a budget line
+
+function addLine() {
+    return null
+}
+
+//update a given line
+function updateLine() {
+    return null
+}
+
+//delete a budget line 
+function deleteLine() {
+    return null
 }
