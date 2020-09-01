0b70b9d0c6f065d051dbc073a7c629fbb0bb10c7
Victoria  Topham <topham.victoria@gmail.com>
2020-09-01 14:28:52 -0700

    code cleanup

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-// const express = require('express')
 
 const app = require('./api/app.js');
-// const server = express();
+
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
 app.timeout = 60 * 10 * 1000;
 
-
-// const BudgetCategoriesRouter = require('./api/budgetcategories/budgetCategoriesRouter')
-// server.use('/api/budgetcategories', BudgetCategoriesRouter)
