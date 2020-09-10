760645d35534e945aa52816d9bb910840708e45e
Victoria  Topham <topham.victoria@gmail.com>
2020-09-10 12:26:42 -0700

    trying to change some capitalization to see if it will fix a testing issue

--- api/categories/categoriesRouter.js
+++ api/categories/categoriesRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const Categories = require('./CategoriesModel');
+const Categories = require('./categoriesModel.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
