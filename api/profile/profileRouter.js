4acc24d3dd2826a5585a207bf76620028055aadb
unknown <locbgiang@gmail.com>
2020-09-23 16:39:47 -0700

    fix lint

--- api/profile/profileRouter.js
+++ api/profile/profileRouter.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const express = require('express');
-const authRequired = require('../middleware/authRequired');
+//const authRequired = require('../middleware/authRequired');
 const Profiles = require('./profileModel');
 const router = express.Router();
 
